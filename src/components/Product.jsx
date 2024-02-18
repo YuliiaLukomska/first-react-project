5ad3c329bea9a58d0605da4f7525f2ba12180d7c
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-02-18 21:34:25 +0200

    add props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,14 @@
-// це оголошення компонента. Він не отримує props (властивість), а просто повертає розмітку
-const Product = () => {
+// це оголошення компонента. Компонент - це функція. Зараз він не отримує props (властивість), а просто повертає розмітку. Але може отримувати.
+const Product = ({
+  name,
+  imgUrl = '<https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder>',
+  price,
+}) => {
   return (
     <div>
-      <h2>Cookies</h2>
-      <p>Price: 999 credits</p>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="640" />
+      <p>Price: {price} credits</p>
     </div>
   );
 };
