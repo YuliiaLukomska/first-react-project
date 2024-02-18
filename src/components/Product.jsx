d5621b14ac7b3c22e74f39a85f41ca668167474f
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-02-18 20:34:49 +0200

    create components

---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+// це оголошення компонента. Він не отримує props (властивість), а просто повертає розмітку
+const Product = () => {
+  return (
+    <div>
+      <h2>Cookies</h2>
+      <p>Price: 999 credits</p>
+    </div>
+  );
+};
+
+export default Product;
